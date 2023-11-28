3d608798df3ced95418737eb825282af0741bd19
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-11-28 21:39:39 +0300

    finish home page

--- team_7/src/pages/index.js
+++ team_7/src/pages/index.js
@@ -2,9 +2,85 @@ import Head from "next/head"
 import Image from "next/image"
 // import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
+import { useEffect, useState } from "react"
+import Link from "next/link"
 
 // const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  return <div>Hello</div>
+  //store the latest movies
+  const [latestMovies, setLatestMovies] = useState([])
+  //options for the fetch request
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjcxYTRhY2NkMGUwY2I0NzBmYWZkMjlhMmJjOTZjNiIsInN1YiI6IjY1NjYwODU3YTM0OTExMDExYjU5MTk2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bd7zqZOCEOyovLHdwMIyHB6BX_EgPzxw6JCCTiLNriQ",
+    },
+  }
+
+  async function getLatestMovies() {
+    const resp = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options,
+    )
+    const data = await resp.json()
+    setLatestMovies([...data.results])
+  }
+
+  useEffect(() => {
+    getLatestMovies()
+  }, [])
+
+  console.log(latestMovies[0])
+  let movies
+  movies = latestMovies.map((movie, index) => {
+    return (
+      <div
+        key={index}
+        style={{
+          backgroundColor: "#999",
+          width: "150px",
+          margin: "10px auto",
+          padding: "2px",
+        }}
+      >
+        <li
+          style={{
+            margin: "10px",
+          }}
+        >
+          <Link href={`./movies/${movie.id}`}>
+            <img
+              width={"100px"}
+              height={"100px"}
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            />
+          </Link>
+          <br />
+          {movie.title}
+        </li>
+      </div>
+    )
+  })
+  return (
+    <div style={{ textAlign: "center" }}>
+      Latest Movies
+      <div style={{ margin: "30px auto" }}>
+        <ul
+          style={{
+            margin: "10px",
+            listStyle: "none",
+            padding: "0",
+            margin: "10px auto",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          {true && movies}
+        </ul>
+      </div>
+    </div>
+  )
 }
