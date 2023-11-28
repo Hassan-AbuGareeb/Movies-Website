250681862bab86966a0052951cdd6c0f4d076980
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-11-28 14:46:44 +0300

    Build: finish intital project setup
    finish the project setup and add all the packages.

---
+++ team_7/src/util/API.js
@@ -0,0 +1,8 @@
+// This contains all the documentation you need.
+Documentation = "https://developer.themoviedb.org/docs"
+
+// This contains the references to the API endpoints
+API_References = "https://developer.themoviedb.org/reference/"
+
+// This contains the Baseurl that you will use to interact with the API.'
+API_BASEURL = "https://api.themoviedb.org/3"
