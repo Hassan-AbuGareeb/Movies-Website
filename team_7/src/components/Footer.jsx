05424e2134e6d5d9e3965f9a032f723aa0159373
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-12-04 19:11:33 +0300

    fixed some issues with nav bar

--- team_7/src/components/Footer.jsx
+++ team_7/src/components/Footer.jsx
@@ -1,26 +1,29 @@
-import styles from './Footer.module.css';
-import React from 'react';
+import styles from "../styles/footer.module.css"
+import React from "react"
 
 const developers = [
   {
-    name: 'Muna Al Haj Eid',
-    photo: 'https://drive.google.com/file/d/1zoElnYHwTMOtGojVqVVwSTVFWrdMdt9E/view?usp=sharing',
-    linkedin: 'https://www.linkedin.com/in/monabadei/',
-    github: 'https://github.com/MonaAlHajEid',
+    name: "Muna Al Haj Eid",
+    photo:
+      "https://drive.google.com/file/d/1zoElnYHwTMOtGojVqVVwSTVFWrdMdt9E/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/monabadei/",
+    github: "https://github.com/MonaAlHajEid",
   },
   {
-    name: 'Jana Abuhaltam',
-    photo: 'https://drive.google.com/file/d/1adexxgVbn79z42KvSVDVKRe1_5jYi64L/view?usp=sharing',
-    linkedin: 'https://www.linkedin.com/in/jana-abuhaltam-b00335288/',
-    github: 'https://github.com/JanaAbuHaltam',
+    name: "Jana Abuhaltam",
+    photo:
+      "https://drive.google.com/file/d/1adexxgVbn79z42KvSVDVKRe1_5jYi64L/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/jana-abuhaltam-b00335288/",
+    github: "https://github.com/JanaAbuHaltam",
   },
   {
-    name: 'Hassan AbuGareeb',
-    photo: 'https://drive.google.com/file/d/1dkUfL5DWT4gSaiohuYN1Sw095XpDFBz0/view?usp=sharing',
-    linkedin: 'https://www.linkedin.com/in/hassan-abu-ghareeb-a13b1126a/',
-    github: 'https://github.com/Hassan-AbuGareeb',
+    name: "Hassan AbuGareeb",
+    photo:
+      "https://drive.google.com/file/d/1dkUfL5DWT4gSaiohuYN1Sw095XpDFBz0/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/hassan-abu-ghareeb-a13b1126a/",
+    github: "https://github.com/Hassan-AbuGareeb",
   },
-];
+]
 
 const Footer = () => {
   return (
@@ -31,10 +34,18 @@ const Footer = () => {
             <img src={developer.photo} alt={`Developer ${index + 1}`} />
             <p>{developer.name}</p>
             <div className={styles.links}>
-              <a href={developer.linkedin} target="_blank" rel="noopener noreferrer">
+              <a
+                href={developer.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 LinkedIn
               </a>
-              <a href={developer.github} target="_blank" rel="noopener noreferrer">
+              <a
+                href={developer.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 GitHub
               </a>
             </div>
@@ -42,14 +53,14 @@ const Footer = () => {
         ))}
       </div>
       <p>
-        Connect with us on social media for the
-        latest updates and more movie magic!
+        Connect with us on social media for the latest updates and more movie
+        magic!
       </p>
-      <p>© {new Date().getFullYear()} Movie Project Team 7. All rights reserved.</p>
+      <p>
+        © {new Date().getFullYear()} Movie Project Team 7. All rights reserved.
+      </p>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
-
-
+export default Footer
--- team_7/src/components/Navbar.jsx
+++ team_7/src/components/Navbar.jsx
@@ -1,89 +1,80 @@
 import React, { useEffect, useState } from "react"
-import styles from "./Navbar.module.css"
-import { getServerSideProps } from "next/dist/build/templates/pages"
+import styles from "../styles/navbar.module.css"
 import Link from "next/link"
-
-function onSearchChange(event) {
-  setSearch(event.target.value)
-}
 
 const Navbar = ({ genres }) => {
   const [search, setSearch] = useState("")
   const movieFilters = ["popular", "top_rated", "upcoming", "now_playing"]
-  const Navbar = ({ genres }) => {
-    const [search, setSearch] = useState("")
-    const movieFilters = ["popular", "top_rated", "upcoming", "now_playing"]
-    return (
-      <nav className={styles.navbar}>
-        {/* Logo */}
-        <div className={styles.logo}>Team 7</div>
+  return (
+    <nav className={styles.navbar}>
+      {/* Logo */}
+      <div className={styles.logo}>Team 7</div>
 
-        {/* Genres Dropdown */}
-        <div className={styles.dropdown}>
-          <button className={styles.dropbtn}>Genres</button>
-          <div className={styles.dropdownContent}>
-            {genres.map((genre) => (
-              <Link
-                href={{
-                  pathname: "../pages/movies",
-                  query: { page: 1, genre: [genre.name] },
-                }}
-              >
-                {" "}
-                {genre.name}
-              </Link>
-            ))}
-          </div>
-        </div>
+      {/* Genres Dropdown */}
+      <div className={styles.dropdown}>
+        <button className={styles.dropbtn}>Genres</button>
+        {/* <div className={styles.dropdownContent}>
+          {genres.map((genre) => (
+            <Link
+              href={{
+                pathname: "../pages/movies",
+                query: { page: 1, genre: [genre.name] },
+              }}
+            >
+              {" "}
+              {genre.name}
+            </Link>
+          ))}
+        </div> */}
+      </div>
 
-        {/* Movies Dropdown */}
-        <div className={styles.dropdown}>
-          <button className={styles.dropbtn}>Movies</button>
-          <div className={styles.dropdownContent}>
-            {movieFilters.map((movie, index) => (
-              <Link
-                key={index}
-                href={{
-                  pathname: "../pages/movies",
-                  query: { page: 1, filter: [movie] },
-                }}
-              >
-                {movie}
-              </Link>
-            ))}
-          </div>
-        </div>
+      {/* Movies Dropdown */}
+      <div className={styles.dropdown}>
+        <button className={styles.dropbtn}>Movies</button>
+        {/* <div className={styles.dropdownContent}>
+          {movieFilters.map((movie, index) => (
+            <Link
+              key={index}
+              href={{
+                pathname: "../pages/movies",
+                query: { page: 1, filter: [movie] },
+              }}
+            >
+              {movie}
+            </Link>
+          ))}
+        </div> */}
+      </div>
 
+      <Link
+        href={{
+          pathname: "/actors",
+          query: { page: 1 },
+        }}
+      >
+        <div>Actors</div>
+      </Link>
+
+      {/* Search Box */}
+      <div className={styles.searchBox}>
+        <input
+          type="text"
+          placeholder="Search for movies or actors"
+          className={styles.searchInput}
+          value={search}
+          onChange={setSearch}
+        />
         <Link
           href={{
-            pathname: "/actors",
-            query: { page: 1 },
+            pathname: "/search",
+            query: { serchValue: [search] },
           }}
         >
-          <div>Actors</div>
+          <div className={styles.searchButton}>Search</div>
         </Link>
-
-        {/* Search Box */}
-        <div className={styles.searchBox}>
-          <input
-            type="text"
-            placeholder="Search for movies or actors"
-            className={styles.searchInput}
-            value={search}
-            onChange={onSearchChange}
-          />
-          <Link
-            href={{
-              pathname: "/search",
-              query: { serchValue: [search] },
-            }}
-          >
-            <div className={styles.searchButton}>Search</div>
-          </Link>
-        </div>
-      </nav>
-    )
-  }
+      </div>
+    </nav>
+  )
 }
 export default Navbar
 
