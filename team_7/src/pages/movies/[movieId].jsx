2fdb99b639076988574e64c1a2adbfb08595f127
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-12-06 16:12:58 +0300

    intial movie single page styling

--- team_7/src/pages/movies/[movieId].jsx
+++ team_7/src/pages/movies/[movieId].jsx
@@ -21,83 +21,99 @@ const MoviePage = ({
   } = movieInfo
 
   const actorsItems = mainActors.map((actor, index) => (
-    <div key={index}>
+    <div
+      key={index}
+      className="transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 text-center max-w-lg"
+    >
       <Link href={`../actors/${actor.id}`} key={index}>
         <img
-          width={"100px"}
+          className="h-56 w-40 rounded-lg shadow-xl  "
           src={`https://image.tmdb.org/t/p/original/${actor.profile_path}`}
         />
+        <span className=" text-slate-300 font-semibold break-normal">
+          {actor.name}
+        </span>
       </Link>
-      <p>{actor.name}</p>
     </div>
   ))
 
   const relatedMoviesItems = relatedMovies.map((movie, index) => (
-    <div key={index}>
-      <Link href={`../movies/${movie.id}`} key={index}>
+    <div
+      key={index}
+      className="transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 text-center max-w-[160px]"
+    >
+      <Link href={`/movies/${movie.id}`} key={index} passHref>
         <img
-          width={"100px"}
+          className="h-56 w-40 rounded-lg shadow-xl  "
           src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         />
       </Link>
-      <p>{movie.title}</p>
+      <span className=" text-slate-300 font-semibold break-normal">
+        {movie.title}
+      </span>
     </div>
   ))
-
+  const hours = Math.floor(runtime / 60)
+  const minutes = runtime % 60
   const productionCompany =
     production_companies.length !== 0 ? production_companies[0] : null
 
   return (
-    <div>
-      <div className="bg-green-500 flex">
+    <div className="bg-slate-600 ">
+      <div className="bg-slate-600 flex p-5 my-16 gap-6 text-slate-200">
         {!!poster && (
           <img
             src={`https://image.tmdb.org/t/p/original/${poster}`}
-            className="w-96 h-1/4 rounded-xl"
+            className="w-[300px] h-[400] rounded-2xl border-"
           />
         )}
-        <div className="flex flex-col justify-end">
-          <p>{title}</p>
-          <p>released on: {releaseDate}</p>
-          <p>duration: {runtime}</p>
-          <p>language: {language}</p>
-          <p>rating: {rating}</p>
-          <p>votings: {votings}</p>
-          <p>
+        <div className="flex flex-col justify-evenly ">
+          <p className="text-4xl ">{title}</p>
+          <p className="text-2xl">release in: {releaseDate.slice(0, 4)}</p>
+          <p className="text-2xl">
+            duration: {hours}
+            <span className="text-lg ">h </span>
+            {minutes}
+            <span className="text-lg ">m</span>
+          </p>
+          <p className="text-2xl">language: {language}</p>
+          <p className="text-2xl">rating: {Math.round(rating * 10) / 10}</p>
+          <p className="text-2xl">votings: {votings}</p>
+          <p className="text-2xl">
             Directed by: {directors.map((director) => director.name).join(", ")}
           </p>
-          <p>overview: {overview}</p>
+          <p className="text-xl max-w-l">
+            <span className="text-2xl font-semibold">overview:</span>
+            <br /> {overview}
+          </p>
         </div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          justifyContent: "space-around",
-        }}
-      >
-        {actorsItems}
+      <div className="flex flex-col mx-20 gap-10 my-16">
+        <p className="text-slate-100 text-4xl font-bold">Cast</p>
+        <div className="flex gap-9 justify-around ">{actorsItems}</div>
       </div>
-      <hr />
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          justifyContent: "space-around",
-        }}
-      >
-        {relatedMoviesItems}
+      <div className="flex flex-col mx-20 gap-10 my-16">
+        <p className="text-slate-100 text-4xl font-bold">Similar movies</p>
+        <div className="flex gap-9 justify-around ">{relatedMoviesItems}</div>
       </div>
-      {trailerLink && (
-        <iframe src={trailerLink} width={"400px"} height={"250px"}></iframe>
-      )}
+
+      <div className="mx-20 mb-16 ">
+        <p className=" text-slate-100 text-4xl font-bold mb-8">Trailer</p>
+
+        {trailerLink && (
+          <iframe
+            src={trailerLink}
+            className=" mx-auto rounded-xl w-[700px] h-[400px]"
+          ></iframe>
+        )}
+      </div>
       {productionCompany && (
-        <div>
-          produced by:
-          {productionCompany.name}
+        <div className="flex mx-20 pb-10 text-slate-100 font-semibold text-2xl ">
+          <span className="mt-auto">produced by:</span>
           <img
-            width={"200px"}
             src={`https://image.tmdb.org/t/p/original/${productionCompany.logo_path}`}
+            alt={productionCompany.name}
+            className="w-auto h-20 ml-10"
           />
         </div>
       )}
@@ -130,7 +146,7 @@ export async function getServerSideProps(context) {
   const directors = creditsData.crew.filter(
     (person) => person.job === "Director",
   )
-  const mainActors = creditsData.cast.slice(0, 5)
+  const mainActors = creditsData.cast.slice(0, 6)
 
   const similarMoviesResponse = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`,
